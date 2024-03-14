406d599ccfd3364b0543371f07872b188c0015e6
mikhail "synzr <mikhail@synzr.space>
2024-03-14 10:24:34 +0500

    Length of last word without LINQ

--- 58-length-of-last-word.cs
+++ 58-length-of-last-word.cs
@@ -1,9 +1,23 @@
 public class Solution {
     public int LengthOfLastWord(string s) {
-        return s
-            .Trim()
-            .Split(' ', StringSplitOptions.RemoveEmptyEntries)
-            .Last()
-            .Length;
+        int characterCount = 0;
+
+        for (int index = s.Length - 1; index >= 0; index--)
+        {
+            char character = s[index];
+
+            if (character != ' ')
+            {
+                characterCount++;
+                continue;
+            }
+
+            if (character == ' ' && characterCount > 0)
+            {
+                break;
+            }
+        }
+
+        return characterCount;
     }
 }
